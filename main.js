b2893c93768a89b1cccd75c3bb629a6420b91246
norkitorki <norki@tuta.io>
2023-07-14 09:08:39 +0200

    Query needed nodes

---
+++ main.js
@@ -0,0 +1,6 @@
+const newBook           = document.querySelector('.new-book');
+const form              = document.forms['bookForm'];
+const sortTable         = document.querySelector('.table-sort');
+const tableBody         = document.querySelector('tbody');
+const newBookForm       = document.querySelector('form');
+const newBookFormSubmit = newBookForm.querySelector('button[type=submit]');
