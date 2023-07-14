21e8dbb9c184463c5603ab6e0d78710de2a34fd8
norkitorki <norki@tuta.io>
2023-07-14 09:09:38 +0200

    Create book constructor function

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ const newBookForm       = document.querySelector('form');
 const newBookFormSubmit = newBookForm.querySelector('button[type=submit]');
 
 let library = [];
+
+function Book(title, author, pages, year, read, id = null) {
+  this.title = title;
+  this.author = author;
+  this.pages = Number(pages);
+  this.year = Number(year);
+  this.read = read;
+  this.id = id;
+};
