c251c8495702235212155bb359f42b5ce62e1ad5
norkitorki <norki@tuta.io>
2023-07-22 08:02:24 +0200

    Derive the id from internal bookCount

--- script/book.js
+++ script/book.js
@@ -1,11 +1,26 @@
 class Book {
-  constructor(title, author, pages, year, read, id) {
+  static bookCount() {
+    return Number(localStorage.getItem('bookCount'));
+  }
+
+  static updateBookCount(step = Book.bookCount() + 1) {
+    localStorage.setItem('bookCount', step);
+  }
+
+  static resetBookCount() {
+    return localStorage.setItem('bookCount', 0);
+  }
+
+  constructor(title, author, pages, year, read) {
+    if (!Book.bookCount()) Book.resetBookCount();
+    Book.updateBookCount();
+
     this.title  = title;
     this.author = author;
     this.pages  = Number(pages);
     this.year   = Number(year);
     this.read   = read;
-    this.id     = Number(id) || null;
+    this.id     = Number(Book.bookCount());
   }
 
   update(property, value) {
